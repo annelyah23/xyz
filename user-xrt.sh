67068d656b81e480547f0f615e150bc7b7d642ae
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-17 14:28:48 +0800

    Update user-xrt.sh

--- user-xrt.sh
+++ user-xrt.sh
@@ -6,21 +6,29 @@
 # (C) Copyright 2022
 # =========================================
 clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+purple='\e[0;35m'
+NC='\e[0m'
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/xtrojan.json")
         if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-                echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\\E[0;41;36m  Check XRAY Trojan TCP XTLS Config  \E[0m"
-                echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+                echo -e "\033[0;34m╔============================================╗\033[0m"
+                echo -e "\e[0;35m      Check XRAY Trojan TCP XTLS Config  \e[0m"
+                echo -e "\033[0;34m╚============================================╝\033[0m"
                 echo ""
                 echo "You have no existing clients!"
                 exit 1
         fi
 
-        echo ""
+        echo -e "\033[0;34m╔============================================╗\033[0m"
+        echo -e "\e[0;35m      Check XRAY Trojan TCP XTLS Config   \e[0m"
+        echo -e "\033[0;34m╚============================================╝\033[0m"
         echo " Select the existing client to view the config"
         echo " Press CTRL+C to return"
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\033[0;34m============================================\033[0m"
         echo "     No  Expired   User"
         grep -E "^### " "/usr/local/etc/xray/xtrojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
         until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
