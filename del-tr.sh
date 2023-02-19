b32d068a16a23e8b9feccfb80891b15627f9a83d
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-19 10:03:54 +0800

    Update del-tr.sh

--- del-tr.sh
+++ del-tr.sh
@@ -15,7 +15,7 @@ NC='\e[0m'
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/trojanws.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
         echo -e "\033[0;34m╔===========================================╗\033[0m"
-        echo -e "\e[0;35m           Delete XRAY Trojan WS Account   \033[0m"
+        echo -e "\e[0;35m           ${bold}Delete Xray Trojan WS Account   \033[0m"
         echo -e "\033[0;34m╚===========================================╝\033[0m"
 		echo ""
 		echo "You have no existing clients!"
@@ -23,7 +23,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/trojanws.json")
 		exit 1
 	fi
         echo -e "\033[0;34m╔===========================================╗\033[0m"
-        echo -e "\e[0;35m'       Delete XRAY Trojan WS Account   \033[0m"
+        echo -e "\e[0;35m'       ${bold}Delete Xray Trojan WS Account   \033[0m"
         echo -e "\033[0;34m╚===========================================╝\033[0m"
 	
 	echo " Select the existing client you want to remove"
