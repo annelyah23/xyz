f7920ed7673ee246f4d5ede589b4d67aa4039718
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-19 18:12:23 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -15,48 +15,41 @@ clear
 IP=$(wget -qO- icanhazip.com);
 date=$(date +"%Y-%m-%d")
 clear
-echo " VPS Data Backup By JsPhantom "
+echo " Enter Your Email To Receive Message"
+read -rp " Email: " -e email
 sleep 1
-echo ""
-echo -e "[ ${green}INFO${NC} ] Please Insert Password To Secure Backup Data ."
-echo ""
-read -rp "Enter password : " -e InputPass
+echo Directory Created
+mkdir /root/backup
+sleep 1
+echo Start Backup
 clear
-sleep 1
-if [[ -z $InputPass ]]; then
-exit 0
-fi
-echo -e "[ ${green}INFO${NC} ] Processing . . . "
-mkdir -p /root/backup
-sleep 1
-clear
-echo " Please Wait VPS Data Backup In Progress . . . "
-cp -r /root/.acme.sh /root/backup/ &> /dev/null
-cp -r /var/lib/premium-script/ /root/backup/premium-script
-cp -r /usr/local/etc/xray /root/backup/xray
-cp -r /usr/local/etc/xray/*.json /root/backup/ >/dev/null 2>&1
-cp -r /home/vps/public_html /root/backup/public_html
-cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
-cp -r /etc/crontab /root/backup/crontab &> /dev/null
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cp -r /etc/wireguard backup/wireguard
+cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
+cp -r /var/lib/premium-script/ backup/premium-script
+cp -r /usr/local/etc/xray backup/xray
+cp -r /etc/trojan-go backup/trojan-go
+cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
+cp -r /home/vps/public_html backup/public_html
 cd /root
-zip -rP $InputPass $IP-$date.zip backup > /dev/null 2>&1
+zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
 url=$(rclone link dr:backup/$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-clear
-echo -e "\033[1;37mVPS Data Backup By JsPhantom\033[0m
-\033[1;37mTelegram : https://t.me/JsPhantom / @JsPhantom\033[0m"
-echo ""
-echo "Please Copy Link Below & Save In Notepad"
-echo ""
-echo -e "Your VPS IP ( \033[1;37m$IP\033[0m )"
-echo ""
-echo -e "Your VPS Data Backup Password : \033[1;37m$InputPass\033[0m"
-echo ""
-echo -e "\033[1;37m$link\033[0m"
-echo ""
-echo "If you want to restore data, please enter the link above"
+echo -e "The following is a link to your vps data backup file.
+
+Your VPS IP $IP
+
+$link
+
+If you want to restore data, please enter the link above.
+
+Thank You For Using Our Services" | mail -s "Backup Data" $email
 rm -rf /root/backup
 rm -r /root/$IP-$date.zip
-echo ""
+echo "Done"
+echo "Please Check Your Email"
