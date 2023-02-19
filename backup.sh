514bc1d84a62824ebcd4cffc9c427862bfea439f
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-19 21:02:51 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -122,5 +122,4 @@ echo -e "$COLOR1┌────────────────────�
 echo -e "$COLOR1│${NC}            •   JsPhantom-VPN   •                "
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo
-read -n 1 -s -r -p "   Press any key to back on menu"
-menu-backup
+read -n 1 -s -r -p "Press any key to back on menu"
